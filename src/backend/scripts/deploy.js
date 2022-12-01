6e7a165a20c64e9295ce59554b1a19f49167523c
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-01 16:05:32 +0700

    Refactor

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -5,39 +5,15 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
+  const vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D // Goerli
+  // const vrfCoordinator = 0x271682DEB8C4E0901D1a1550aD2e64D568E69909 // Mainnet
+  const subscriptionId = 256 // Goerli
 
-  // vrfCoordinator Goerli: 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
-  // vrfCoordinator Mainnet: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909
+  const Raffle = await ethers.getContractFactory("Raffle");
+  const raffle = await Raffle.deploy(vrfCoordinator, subscriptionId);
+  console.log("Raffle contract address", raffle.address)
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  console.log("Token contract address", token.address)
-
-  const House = await ethers.getContractFactory("House");
-   //newOwner, admins, tokenAddress
-  // client's goerli wallet: 0xe2a183EC51E30757DF6C12F43262e6D956B95561
-  // const house = await House.deploy("0xCaC8c3f44f913b012D304d36E94BA124B1Ca8A9B", // mumbai
-  //   ["0xCaC8c3f44f913b012D304d36E94BA124B1Ca8A9B", "0xCaC8c3f44f913b012D304d36E94BA124B1Ca8A9B"], token.address);
-  const house = await House.deploy("0xe2a183EC51E30757DF6C12F43262e6D956B95561", // goerli
-    ["0xe2a183EC51E30757DF6C12F43262e6D956B95561", "0xD71E736a7eF7a9564528D41c5c656c46c18a2AEd"], token.address);
-    // const house = await House.deploy("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", // localhost
-    //   ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"], token.address);
-  console.log("House contract address", house.address)
-
-  const CoinFlip = await ethers.getContractFactory("CoinFlip");
-  const coinflip = await CoinFlip.deploy(house.address, 765); // goerli
-  // const coinflip = await CoinFlip.deploy(house.address, 1739); // mumbai
-  console.log("CoinFlip contract address", coinflip.address)
-
-  await house.setGameContracts([coinflip.address]);
-  await token.claimInitialSupply(house.address);
-  console.log("Functions called")
-  
-  
-  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles(token, "Token");
-  saveFrontendFiles(house, "House");
-  saveFrontendFiles(coinflip, "CoinFlip");
+  saveFrontendFiles(raffle, "Raffle");
 }
 
 function saveFrontendFiles(contract, name) {
